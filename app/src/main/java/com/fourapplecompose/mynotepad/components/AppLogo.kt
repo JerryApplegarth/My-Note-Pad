9320187b53a2c67ca06f537a3d18e860bc0029f0
Jerry Applegarth <applegarth1630@gmail.com>
2022-02-08 11:23:37 -0800

    first main commit

--- app/src/main/java/com/fourapplecompose/mynotepad/components/AppLogo.kt
+++ app/src/main/java/com/fourapplecompose/mynotepad/components/AppLogo.kt
@@ -2,15 +2,22 @@ package com.fourapplecompose.mynotepad.components
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -39,10 +46,45 @@ fun AppLogo() {
             fontWeight = FontWeight.Bold,
             color = MaterialTheme.colors.mediumGreen
         )
-        Text(
-            text = stringResource(R.string.but_me_coffee),
-            modifier = Modifier.padding(4.dp)
+
+        val annotatedLinkString: AnnotatedString = buildAnnotatedString {
+
+            val str = "Buy me a coffee"
+            val startIndex = str.indexOf("Coffee")
+            val endIndex = startIndex + 6
+            append(str)
+            addStyle(
+                style = SpanStyle(
+                    color = Color(0xff64B5F6),
+                    fontSize = 18.sp,
+                    textDecoration = TextDecoration.Underline
+                ), start = startIndex, end = endIndex
+            )
+
+            // attach a string annotation that stores a URL to the text "link"
+            addStringAnnotation(
+                tag = "URL",
+                annotation = "https://www.buymeacoffee.com/JerryApplegarth ",
+                start = startIndex,
+                end = endIndex
+            )
+
+        }
+        val uriHandler = LocalUriHandler.current
+        ClickableText(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+            text = annotatedLinkString,
+            onClick = {
+                annotatedLinkString
+                    .getStringAnnotations("URL", it, it)
+                    .firstOrNull()?.let { stringAnnotation ->
+                        uriHandler.openUri(stringAnnotation.item)
+                    }
+            }
         )
+
         Text(
             text = stringResource(R.string.http_address,
 
